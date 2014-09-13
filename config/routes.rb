306146ee342b08e9a2ef0683b7065f7b9c492a02
thelowdowndev <thelowdowndev@gmail.com>
2014-09-13 18:23:34 -0400

    Updated Static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
+  get "ststic_pages/home"
+  get "ststic_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
