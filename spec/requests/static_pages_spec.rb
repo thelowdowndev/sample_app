e4db624ba3f7ce02d225deea171c251b0717a6ab
thelowdowndev <thelowdowndev@gmail.com>
2014-09-16 20:39:51 -0400

    Added Guard and edited config files

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
 
     it "should have the content 'Sample App' " do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
     it 'should have the right title' do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title( "#{base_title} | Home")
     end
   end
   describe "Help page" do
@@ -23,7 +25,7 @@ describe "StaticPages" do
 
     it 'should have the right title' do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -36,7 +38,20 @@ describe "StaticPages" do
 
     it 'should have the right title' do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/contact'
+      expect(page).to have_title(" #{base_title} | Contact Us")
     end
   end
 end
